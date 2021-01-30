ebf830c5640e6dded9910abde60e2546d04625bd
hatice adıgüzel <adiguzell.hatice@gmail.com>
2021-01-30 18:40:27 +0300

    Add project files.

---
+++ MyDictionary/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary <string> myDictionary = new MyDictionary <string>();
+            myDictionary.Add("Hatice");
+        }
+    }
+}
